9d6f642f97d5a028939c9e3f8488bac17ac3637a
William Carrington <wccarrington@gmail.com>
2011-05-28 22:49:16 -0500

    Added beginning curses support and multiple workers.

--- dominionate.py
+++ dominionate.py
@@ -4,32 +4,44 @@ import sys, subprocess
 import Dominion
 import Interop
 import Modifs
+import curses
+import os
+
+window = curses.initscr()
+rows, cols = window.getmaxyx()
 
 specs = sys.argv[1:]
 
-p = subprocess.Popen(
-    ["./worker.py"]+specs,
-    stdin = subprocess.PIPE,
-    stdout = subprocess.PIPE)
+subprocesses = 2
+
+workers = [subprocess.Popen(
+           ["./worker.py"]+specs,
+           stdin = subprocess.PIPE,
+           stdout = subprocess.PIPE) for x in xrange(subprocesses)]
 
 resultbatch = Dominion.ResultBatch(Modifs.MODIFS)
 
 try:
     while True:
-        resultbatch.add(Interop.recv(p.stdout))
+        for w in workers:
+            resultbatch.add(Interop.recv(w.stdout))
 
-        sys.stdout.write("[H[2J")
+#        sys.stdout.write("[H[2J")
+        window.erase()
+        window.move(0,0)
         basesum = resultbatch.base.summary()
-        print "%-22s: %s" % ("base:", basesum)
+        window.addstr("%-22s: %s" % ("base:", basesum))
         sums = {}
         for m,hist in resultbatch.modif.items():
             sums[m] = hist.summary().sub(basesum)
-        for m in sorted(sums.keys(), key=lambda m: sums[m].money+sums[m].buys, reverse=False):
-            print "%-22s: %s" % (m, sums[m])
+        for n, m in enumerate(sorted(sums.keys(), key=lambda m: sums[m].money+sums[m].buys, reverse=False)[-rows:]):
+            window.move(n, 0)
+            window.addstr("%-22s: %s" % (m, sums[m]))
+        window.refresh()
 
-except KeyboardInterrupt:
-    pass
+finally:
+    curses.endwin()
 
-p.send_signal(2)
-
-print p.wait()
+    for w in workers:
+        w.send_signal(2)
+        print w.wait()
